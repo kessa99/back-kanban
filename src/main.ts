1689ed6d834ab998063bf5e3020a42ef4839051b
kessa99 <kipre325@gmail.com>
2025-09-15 11:50:48 +0000

    add

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     credentials: true,
   });
   
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Server is running on port http://localhost:${process.env.PORT ?? 3000}`);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`Server is running on port http://0.0.0.0:${process.env.PORT ?? 3000}`);
 }
 bootstrap();
